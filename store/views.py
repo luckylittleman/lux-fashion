b8db42a7fa3458da0846fc266f007573852c918c
Lucky <hilaryomondi08@gmail.com>
2026-04-14 17:58:25 +0300

    Add store API-Product and Category endpoints

--- store/views.py
+++ store/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductListView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(is_available=True)
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__slug=category)
+        return queryset
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductSerializer
