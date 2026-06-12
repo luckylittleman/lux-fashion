e38537551a4c57bd71db7f7f3a3bd98ba4d236dc
Lucky <hilaryomondi08@gmail.com>
2026-06-12 12:51:47 +0300

    corrected an error

--- core/validators.py
+++ core/validators.py
@@ -26,13 +26,17 @@ def validate_password_strength(value):
         raise serializers.ValidationError(
             'Password must be at least 6 characters.'
         )
-    if not re.search(r'[A-Za-z]', value):
+    if not re.search(r'[A-Z]', value):
         raise serializers.ValidationError(
-            'Password must contain at least one letter.'
+            'Password must contain at least one uppercase letter.'
         )
     if not re.search(r'\d', value):
         raise serializers.ValidationError(
             'Password must contain at least one number.'
+        )
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', value):
+        raise serializers.ValidationError(
+            'Password must contain at least one special character e.g. !@#$%'
         )
     return value
 
